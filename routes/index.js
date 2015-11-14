11c607ff0d722869c02ecbb45fe987c65ad2f8a1
JenHsuan <ofalpha@gmail.com>
2015-11-14 13:14:13 +0800

    add a scrpit

--- routes/index.js
+++ routes/index.js
@@ -8,15 +8,30 @@ var router = express.Router();
 
 //module.exports = router;
 module.exports = function(app) {
+
+    //Edit by Sean 15.10.12
     app.get('/', function(req, res) {
-        res.render('index', {
-            title: 'Express'
+        res.render('main', {
+            title: 'Main page'
         });
     });
-    app.get('/2', function(req, res) {
-        res.render('index2', {
-            title2: 'Express2'
+    app.get('/register', function(req, res) {
+        res.render('register', {
+            title: 'Register page'
         });
     });
-    //app.use('/users', users);
+    app.post('/register', function(req, res) {});
+    app.get('/login', function(req, res) {
+        res.render('login', {
+            title: 'Login page'
+        });
+    });
+    app.post('/login', function(req, res) {});
+    app.get('/post', function(req, res) {
+        res.render('post', {
+            title: 'Post page'
+        });
+    });
+    app.post('/post', function(req, res) {});
+    app.post('/logout', function(req, res) {});
 };
