fa7c23ec02dfe1fb9e5b879efecf0345bc96e060
JenHsuan <ofalpha@gmail.com>
2015-11-15 00:11:43 +0800

    add windows command

--- backUp.cmd
+++ backUp.cmd
@@ -1,6 +1,6 @@
 @echo off
 set curDir=%cd%
 set /p IP="Please enter destination IP: "
-echo Y | reg delete HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\SshHostKeys
-%curDir%/util/pscp -p -r %curDir%/ ubuntu@%IP%:/home/ubuntu/myhomepage
+@echo Y | reg delete HKEY_CURRENT_USER\Software\SimonTatham\PuTTY\SshHostKeys
+@echo Yes | "%curDir%/util/pscp" -p -r %curDir%/ ubuntu@%IP%:/home/ubuntu/myhomepage
 ::echo %curDir%\
