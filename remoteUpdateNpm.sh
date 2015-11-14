3b1145939df97b064bc82e60d5621d5f9f608a43
JenHsuan <ofalpha@gmail.com>
2015-11-14 13:16:47 +0800

    add a scrpit

---
+++ remoteUpdateNpm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Program:
+#	User inputs his first name and last name.  Program shows his full name.
+# History:
+# 2015/11/14	Sean	First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+sudo rm ~/.ssh/known_hosts
+
+read -p "Please input destination IP:  " IP       # 提示使用者輸入
+
+pwd=$(pwd)
+ssh -t ubuntu@${IP} "cd /home/ubuntu/myhomepage; ./refreshModule.sh" 
+exit 0
